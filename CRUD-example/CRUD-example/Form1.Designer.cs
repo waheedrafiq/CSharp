bab8fc866a62325d7687984b6367af11e4c03f0d
waheedrafiq <waheedrafiq@outlook.com>
2017-09-25 19:58:31 +0100

    CRUD-upload to Git

---
+++ CRUD-example/CRUD-example/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace CRUD_example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblSaveInfo = new System.Windows.Forms.Label();
+            this.txtUpdate = new System.Windows.Forms.TextBox();
+            this.lblUpdate = new System.Windows.Forms.Label();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnUpdate);
+            this.groupBox1.Controls.Add(this.btnRead);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Location = new System.Drawing.Point(443, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 183);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CRUD";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(24, 133);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(141, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "&Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(24, 104);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(141, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "&Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(24, 63);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(141, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(24, 33);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(141, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.BackColor = System.Drawing.SystemColors.Info;
+            this.txtData.Location = new System.Drawing.Point(13, 78);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtData.Size = new System.Drawing.Size(409, 254);
+            this.txtData.TabIndex = 1;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(11, 42);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(262, 33);
+            this.lblDescription.TabIndex = 2;
+            this.lblDescription.Text = "Enter Data to Create File";
+            // 
+            // lblSaveInfo
+            // 
+            this.lblSaveInfo.AutoSize = true;
+            this.lblSaveInfo.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblSaveInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSaveInfo.Font = new System.Drawing.Font("Lucida Calligraphy", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaveInfo.ForeColor = System.Drawing.Color.Red;
+            this.lblSaveInfo.Location = new System.Drawing.Point(443, 277);
+            this.lblSaveInfo.Name = "lblSaveInfo";
+            this.lblSaveInfo.Size = new System.Drawing.Size(42, 47);
+            this.lblSaveInfo.TabIndex = 3;
+            this.lblSaveInfo.Text = "S";
+            // 
+            // txtUpdate
+            // 
+            this.txtUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtUpdate.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUpdate.Location = new System.Drawing.Point(421, 338);
+            this.txtUpdate.Name = "txtUpdate";
+            this.txtUpdate.Size = new System.Drawing.Size(210, 44);
+            this.txtUpdate.TabIndex = 4;
+            this.txtUpdate.Tag = "Updated Word";
+            // 
+            // lblUpdate
+            // 
+            this.lblUpdate.AutoEllipsis = true;
+            this.lblUpdate.AutoSize = true;
+            this.lblUpdate.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpdate.Location = new System.Drawing.Point(12, 346);
+            this.lblUpdate.Name = "lblUpdate";
+            this.lblUpdate.Size = new System.Drawing.Size(96, 29);
+            this.lblUpdate.TabIndex = 5;
+            this.lblUpdate.Text = "Update:";
+            this.lblUpdate.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFind
+            // 
+            this.txtFind.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.txtFind.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFind.Location = new System.Drawing.Point(192, 338);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(210, 44);
+            this.txtFind.TabIndex = 6;
+            this.txtFind.Tag = "Replace Word";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 427);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.lblUpdate);
+            this.Controls.Add(this.txtUpdate);
+            this.Controls.Add(this.lblSaveInfo);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CRUD Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblSaveInfo;
+        private System.Windows.Forms.TextBox txtUpdate;
+        private System.Windows.Forms.Label lblUpdate;
+        private System.Windows.Forms.TextBox txtFind;
+    }
+}
+
---
+++ CRUD-example/CRUD-example/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRUD_example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblSaveInfo.Text = "";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            if(txtData.Text != null)
+            {
+              
+                TextWriter txt = new StreamWriter(path: AppDomain.CurrentDomain.BaseDirectory + @"\" + "userData.txt");
+                txt.Write(txtData.Text);
+                txt.Close(); // close the file stream.
+
+                lblSaveInfo.Text = "S";
+                txtData.Clear();
+            }
+  
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            StreamReader txt = new StreamReader(path: AppDomain.CurrentDomain.BaseDirectory + @"\" + "userData.txt");
+            // read the first line
+            string line = txt.ReadLine();
+
+            // Continue to read EOF 
+            while( line != null)
+            {
+                txtData.AppendText(line + "\r\n");
+                line = txt.ReadLine();
+            }
+            // close the file stream 
+            txt.Close();
+            lblSaveInfo.BackColor = System.Drawing.Color.Azure;
+            lblSaveInfo.Text = "EOF";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string temp = "";
+            List<string> tempList = new List<string>();
+            // StringBuilder newfile = new StringBuilder();
+            using (StreamReader txt = new StreamReader(path: AppDomain.CurrentDomain.BaseDirectory + @"\" + "userData.txt"))
+            {
+
+                // read the first line
+                string line = txt.ReadLine();
+               
+                    while (line != null)
+                    {
+
+                      
+                     Debug.WriteLine("In the IF");
+                     temp = line.Replace(txtFind.Text.Trim(), txtUpdate.Text.Trim());
+                     line = txt.ReadLine();
+                     tempList.Add(temp);
+                          
+
+                    }// end of while read to EOF..
+
+                txt.Close(); // close the stream
+
+                using (StreamWriter txtwrite = new StreamWriter(path: AppDomain.CurrentDomain.BaseDirectory + @"\" + "userData.txt"))
+                {
+                    foreach( string lines in tempList)
+                    {
+                        txtwrite.WriteLine(lines);
+                    }
+                    txtwrite.Close();
+
+                }
+
+
+                lblSaveInfo.BackColor = System.Drawing.Color.Cornsilk;
+                lblSaveInfo.Text = "Updated";
+                txtData.Clear();
+               
+            }
+         
+           
+
+        }// end of update mehtod
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // C# 6.0 using Lambda expressions  
+
+            string usersPath = Path.GetFullPath(path:AppDomain.CurrentDomain.BaseDirectory + @"\" + "userData.txt");
+            string item = txtData.SelectedText.Trim();
+            var lines = File.ReadAllLines(usersPath).Where(line => line.Trim() != item).ToArray();
+            File.WriteAllLines(usersPath, lines);
+
+            lblSaveInfo.BackColor = System.Drawing.Color.DarkGreen;
+            lblSaveInfo.Text = "Deleted";
+            txtData.Clear();
+
+        }// end of Delete method
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        
+        }
+    }// end of class
+}// end of namespace 
